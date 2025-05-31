a560a82b87b8649a1a0daa279f49c793c41518d9
Rika4698 <rikasharmin181@gmail.com>
2025-06-01 01:37:15 +0600

    update resume

--- src/components/PDFDownloadButton.jsx
+++ src/components/PDFDownloadButton.jsx
@@ -4,7 +4,7 @@ const PDFDownloadButton = () => {
   return (
     <>
       <a
-         href="https://drive.google.com/uc?export=download&id=1fYo6hchvULLDmM7dTAMfE6xVJKKw9FL3"
+         href="https://drive.google.com/file/d/1OrILQ_lWw86DRtNqoXpTW8zSe73AG8hj/view?usp=sharing"
         // download="Resume of Sharmin Akter Reka.pdf"
         className="btn btn-sm h-10 bg-sky-500  hover:bg-primary text-white border-none rounded font-medium"
       >
